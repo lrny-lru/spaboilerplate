b3d8912541e21c397f52786fc802d640a2525714
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2021-12-29 15:39:08 -0500

    trouleshoot react-router-dom update 6

--- src/Main.js
+++ src/Main.js
@@ -1,41 +1,34 @@
 import React, { Component } from "react";
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-import Bio from './Bio';
-import Contact from './Contact'
+import { Route,Routes, NavLink} from "react-router-dom";
+import Bio from './components/Bio';
+import Contact from './components/Contact'
 
 //*this is the app frame, an invisible HTML element that acts as the container for your content; can include things like headers, footers, navigation, etc*//
- 
+
 class Main extends Component {
   render() {
     return (
-     <HashRouter>
-        <Routes>
+    
         <div>
+        <main>
           <h1>Simple SPA</h1>
           <ul className="header">
-          <nav>
-            <li><Link to="/">Bio</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </nav>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
-         
-            <Route path="/" component={Bio}/>
-           
-            <Route path="/contact" component={Contact}/>
-          
-
-             
+          <Routes>
+            <Route  path="/" element={<Bio/>}/>
+            
+            <Route path="/contact" element={<Contact/>} />
+          </Routes>
           </div>
+        </main>
         </div>
-        </Routes>
-     </HashRouter>
+      
     );
   }
 }
- 
+
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import Main from "./Main";
+import './index.css';
  
 ReactDOM.render(
-
-  <Main/>, 
+  <BrowserRouter>
+  <Main/>
+  </BrowserRouter>, 
   document.getElementById("root")
 );
